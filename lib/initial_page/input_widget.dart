53c4c3c3c73899137e4dd9b5c284148d1858998e
miltux <moviedo@wacoservices.com>
2021-04-20 12:28:30 -0500

    se integor null safety

--- lib/initial_page/input_widget.dart
+++ lib/initial_page/input_widget.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 class InputWidget extends StatelessWidget {
 
-  final TextEditingController input;
-  final String inputString;
+  final TextEditingController? input;
+  final String? inputString;
 
-  const InputWidget({Key key, this.input, this.inputString}) : super(key: key);
+  const InputWidget({Key? key, this.input, this.inputString}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
